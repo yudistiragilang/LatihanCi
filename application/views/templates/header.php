23580de3f4a23228f75fe3409878e8e3eed88cbd
yudistiragilang <yudhistiragilang22@gmail.com>
2018-08-02 15:30:27 +0700

    first commit

---
+++ application/views/templates/header.php
@@ -0,0 +1,10 @@
+
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CI Latihan</title>
+</head>
+<body>
+    <h1><?php echo $title ?></h1>
+    <h1>Halo Halo</h1>
