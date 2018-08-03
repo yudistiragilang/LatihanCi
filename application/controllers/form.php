2e436adc35741bc07e828f3c5e3a8166815ea04a
yudistiragilang <yudhistiragilang22@gmail.com>
2018-08-03 16:11:51 +0700

    Belajar Form Validation

---
+++ application/controllers/form.php
@@ -0,0 +1,28 @@
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Form extends CI_Controller{
+
+    function __construct(){
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
+    function index(){
+        $this->load->view('v_form');
+    }
+
+    function aksi(){
+        $this->form_validation->set_rules('nama','Nama','required');
+        $this->form_validation->set_rules('email','Email','required');
+        $this->form_validation->set_rules('konfir_email','Konfirmasi Email','required');
+
+        if($this->form_validation->run() !=false){
+            echo "form validation oke";
+        }else{
+            $this->load->view('v_form');
+        }
+
+    }
+}
---
+++ application/views/v_form.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Form Validasi</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1>Membuat Form Validasi dengan CodeIgniter</h1>
+    <?php echo validation_errors(); ?>
+    <!-- form_open(nama class/nama method pada controler) -->
+    <?php echo form_open('form/aksi'); ?>
+        <label for="">Nama</label><br>
+        <input type="text" name="nama"><br>
+        <label for="">Email</label><br>
+        <input type="text" name="email"><br>
+        <label for="">Konfirmasi Email</label><br>
+        <input type="text" name="konfir_email">
+        <input type="submit" value="simpan">
+</body>
+</html>
