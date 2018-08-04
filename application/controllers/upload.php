6321fd55a838abb1f3e6cbe339854b7b7f26b7bd
yudistiragilang <yudhistiragilang22@gmail.com>
2018-08-04 09:11:52 +0700

    tambah komentar sehingga jelas.

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -11,26 +11,26 @@ class Upload extends CI_Controller{
 
     //membuat form upload disini
     function index(){
-        $this->load->view('v_upload',array('error' => ''));
+        $this->load->view('v_upload',array('error' => '')); // menampilkan view v_upload
     }
 
     // fungsi upload
     function aksi_upload(){
-        $config['upload_path']          = './gambar/';
-		$config['allowed_types']        = 'gif|jpg|png';
-		$config['max_size']             = 1000;
-		$config['max_width']            = 10240;
-        $config['max_height']           = 7680;
+        $config['upload_path']          = './gambar/'; //direktori gambar akan disimpan
+		$config['allowed_types']        = 'gif|jpg|png'; //format gambar yang dapat di upload
+		$config['max_size']             = 1000; //ukuran maksimal gambar (Kb)
+		$config['max_width']            = 10240; //ukuran gambar
+        $config['max_height']           = 7680; // ukuran gambar
         
         $this->load->library('upload',$config);
 
         // $this->upload->do_upload('NAME dari form'))
-        if ( ! $this->upload->do_upload('berkas')) {
+        if ( ! $this->upload->do_upload('berkas')) { 
             $error = array('error' => $this->upload->display_errors());
-            $this->load->view('v_upload', $error);
+            $this->load->view('v_upload', $error); //jika data tidak ada redirect ke v_upload 
         }else{
             $data = array('upload_data' => $this->upload->data());
-            $this->load->view('v_upload_sukses', $data);
+            $this->load->view('v_upload_sukses', $data); //jika upload berhasil
         }
 
     }
