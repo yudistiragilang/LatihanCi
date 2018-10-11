e9263bbeb7dfe4c44b40d708f6be88a6b7b5e413
Yudistira Gilang Adisetyo <yudhistiragilang22@gmail.com>
2018-10-11 12:06:15 +0700

    Update ngoding.php

--- application/controllers/ngoding.php
+++ application/controllers/ngoding.php
@@ -4,12 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Ngoding extends CI_Controller{
 
-    // function __construct(){
-    //     parent::__construct();
-    //     $this->load->library('sinaukoding');
-    // }
-
-    
     function index(){
 		$this->load->library('sinaukoding');
 		$this->sinaukoding->nama_saya();
